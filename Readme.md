7aa4fd77e6aedb8d7c1c5e52728238bbdd3c2343
zhuqd <1132154046@qq.com>
2020-11-09 07:19:28 +0800

    🎨  sort(2)

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# 《Javascript 数据结构与算法第三版》学习笔记 :rocket:
--- books/src/ts/algorithm/utils/index.js
+++ books/src/ts/algorithm/utils/index.js
@@ -1,34 +1,88 @@
-export const arr = [2062, 90410, 71989, 30824, 47870, 65359]
+export const arr = [2062, 90410, 71989, 30824, 47870, 11472, 15049, 96799, 65359]
 
-const COMPARE = {
-  BIGGER_THAN: 1,
+// const COMPARE = {
+//   BIGGER_THAN: 1,
+//   LESS_THAN: -1,
+//   EQUAL: 0,
+// }
+
+// export function compareFn(array, a, b) {
+//   if (array[a] > array[b]) return COMPARE.BIGGER_THAN
+//   if (array[a] < array[b]) return COMPARE.LESS_THAN
+// }
+
+// export function swap(array, a, b) {
+//   ;[array[a], array[b]] = [array[b], array[a]]
+// }
+
+// export function findMaxMinValue(arr) {
+//   let max = arr[0]
+//   let min = arr[0]
+
+//   arr.forEach((item) => {
+//     if (item > max) {
+//       max = item
+//     }
+//     if (item < min) {
+//       min = item
+//     }
+//   })
+//   return {
+//     max,
+//     min,
+//   }
+// }
+
+export const Compare = {
   LESS_THAN: -1,
-  EQUAL: 0,
+  BIGGER_THAN: 1,
+  EQUALS: 0,
 }
 
-export function compareFn(array, a, b) {
-  if (array[a] > array[b]) return COMPARE.BIGGER_THAN
-  if (array[a] < array[b]) return COMPARE.LESS_THAN
+export const DOES_NOT_EXIST = -1
+
+export function lesserEquals(a, b, compareFn) {
+  const comp = compareFn(a, b)
+  return comp === Compare.LESS_THAN || comp === Compare.EQUALS
+}
+
+export function biggerEquals(a, b, compareFn) {
+  const comp = compareFn(a, b)
+  return comp === Compare.BIGGER_THAN || comp === Compare.EQUALS
+}
+
+export function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN
+}
+
+export function defaultEquals(a, b) {
+  return a === b
+}
+
+export function defaultToString(item) {
+  if (item === null) {
+    return 'NULL'
+  } else if (item === undefined) {
+    return 'UNDEFINED'
+  } else if (typeof item === 'string' || item instanceof String) {
+    return `${item}`
+  }
+  return item.toString()
 }
 
 export function swap(array, a, b) {
+  /* const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp; */
   ;[array[a], array[b]] = [array[b], array[a]]
 }
+export function reverseCompare(compareFn) {
+  return (a, b) => compareFn(b, a)
+}
 
-export function findMaxMinValue(arr) {
-  let max = arr[0]
-  let min = arr[0]
-
-  arr.forEach((item) => {
-    if (item > max) {
-      max = item
-    }
-    if (item < min) {
-      min = item
-    }
-  })
-  return {
-    max,
-    min,
-  }
+export function defaultDiff(a, b) {
+  return Number(a) - Number(b)
 }
